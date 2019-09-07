77515743bca7f80a7cda7bf7b3299f72f67362af
Melike KILIC <melike180400@gmail.com>
2019-09-07 19:16:30 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,13 +1,13 @@
 
 function callMe() {
   var lyric = "maybe";
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
+return lyric;
 }
-callMe();
+console.log("I just met you...");
+console.log("and this is crazy..");
+console.log("but here's my number..");
+console.log("so call me");
+
 
 
 
